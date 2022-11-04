7ea314455a63438f5099dabea6cf44b6b68a79d5
Radiosmark <dpgarces@uc.cl>
2022-11-04 01:48:34 -0300

    arriba y abajo

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import pygame
 from numpy import *
 import time 
 from scipy.integrate import odeint
-from proyecto import dif_eq
 
 g = 9.81
 ti = 0
-time_scaling = 100.0
+time_scaling = 1.0
 
 class UAV:
-    def __init__(self):
+    def __init__(self, mira):
         self.pos = [400, 400]
         self.theta = pi/2
-        self.long_brazo = 5
+        self.long_brazo = 0.5
         self.masa = 10 #kg
         self.iner = 0.1
         self.V = 0
@@ -21,30 +20,41 @@ class UAV:
         self.bu = 50
         self.bv = 50
         self.bw = 100
-        self.fl = 0
-        self.fr = 0
-
+        self.u1 = 10*g
+        self.u2 = 0
+        self.ti = 0
+        self.mira = mira
 
     def dif_eq(self, x, t):
-        
-        xd0 = x[3]*cos(x[2]) - x[4]*sin(x[2])
-        xd1 = x[3]*sin(x[2]) + x[4]*cos(x[2])
-        xd2 = x[5]
-        xd3 = x[5]*x[4]+(1/self.masa)*(self.fr+self.fl)-(self.bu/self.masa)*x[3]-g*sin(x[2])
-        xd4 = -x[5]*x[3]-(self.bv/self.masa)*x[4]-g*cos(x[2])
-        xd5 = -(self.bw*x[5]/self.iner)+((self.long_brazo*10)/(2*self.iner))*(self.fr-self.fl)
 
-        return multiply(time_scaling , [xd0 ,xd1 ,xd2 ,xd3 ,xd4 ,xd5])
-    
+        ## calculamos los fr y fl
+        nrow1 = [1, 1]
+        nrow2 = [self.long_brazo/2, -self.long_brazo/2]
+        matrix = array([nrow1, nrow2])
+        cons = array([self.u1, self.u2])
+        solution  = linalg.solve(matrix, cons)
+        
+        fr = solution[0]
+        fl = solution[1]
+        
+        xd0 = x[3]*cos(x[2])-x[4]*sin(x[2])
+        xd1 = x[3]*sin(x[2])+x[4]*cos(x[2])
+        xd2 = x[5]
+        xd3 = x[5]*x[4]+(1/self.masa)*(fr+fl)-((self.bu*x[3])/self.masa)-g*sin(x[2])
+        xd4 = -x[5]*x[4]-((self.bv*x[4])/self.masa)-g*cos(x[2])
+        xd5 = ((fr-fl)/(self.iner))-(self.bw/self.iner)*self.w
+
+        return multiply(time_scaling, [xd0, xd1, xd2, xd3, xd4, xd5])
+
     def init(self):
-        global ti
-        Ts = time.time() - ti
+
+        Ts = time.time() - self.ti
         x = array([self.pos[0], self.pos[1], self.theta, self.U, self.V, self.w])
         t = linspace(0, Ts, 2)
-        ti = time.time()
+        self.ti = time.time()
         print("el tiempo es", Ts)
 
-        sol = odeint(dif_eq, x, t)
+        sol = odeint(self.dif_eq, x, t)
         print(sol[-1])
         self.pos[0] = sol[-1, 0]
         self.pos[1] = sol[-1,1]
@@ -53,16 +63,14 @@ class UAV:
         self.V = sol[-1,4]
         self.w = sol[-1,5]
 
-    def update(self):
-        pass
-
     def brazo_derecho(self):
-        return(self.pos[0]-self.long_brazo*5, (800-self.pos[1]))
+        return(self.pos[0]-self.long_brazo*50, (800-self.pos[1]))
 
     def brazo_izquierdo(self):
-        return(self.pos[0]+self.long_brazo*5, (800-self.pos[1]))
+        return(self.pos[0]+self.long_brazo*50, (800-self.pos[1]))
 
     def draw(self, dest):
+
         x = self.pos[0]
         y = self.pos[1]
         rx , ry = self.brazo_derecho()
@@ -72,6 +80,16 @@ class UAV:
         pygame.draw.circle(dest, (0, 255, 0), (lx, ly), 5)
         pygame.draw.circle(dest, (0, 0, 255), (rx, ry), 5)
 
+        ### imprimer la mira
+        self.mira.draw(dest)
+
+class Mira():
+    def __init__(self):
+        self.pos = [200, 200]
+
+    def draw(self, dest):
+        pygame.draw.line(dest, (255,255,255), (self.pos[0]-20, self.pos[1]),(self.pos[0]+20, self.pos[1]))
+        pygame.draw.line(dest, (255,255,255), (self.pos[0], self.pos[1]-20),(self.pos[0], self.pos[1]+20))
 
 def main():
     pygame.init()
@@ -79,32 +97,31 @@ def main():
     ventana = pygame.display.set_mode((800 ,800))
     simulacion = True
 
-    uav = UAV()
+    mira = Mira()
+    uav = UAV(mira)
     uav.init()
     while simulacion:
-        uav.init()
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:  ##  reconoce el input del mouse
+                mira.pos[0] = event.pos[0]  #cambia la posición en x de la mira 
+                mira.pos[1] = event.pos[1]  #cambia la posición en y de la mira 
             if event.type == pygame.QUIT:
                 simulacion = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    uav.fl=100
-                    uav.fr=200
-                if event.key == pygame.K_s:
-                    uav.pos[1]+=10
-                    print("abajo")
-                if event.key == pygame.K_d:
-                    uav.pos[0]+=10
-                    print("derecha")
-                if event.key == pygame.K_a:
-                    uav.pos[0]-=10
-                    print("izquierda")
+                if event.key == pygame.K_UP:
+                    uav.u1 = 2000
+                if event.key == pygame.K_DOWN:
+                    uav.u1 = -2000
+                if event.key == pygame.K_LEFT:
+                    uav.u2 += 0.1
+                if event.key == pygame.K_RIGHT:
+                    uav.u2 += 0.1
 
-            
-            ventana.fill((0,0,0))
-            uav.draw(ventana)
-            pygame.display.flip()
-            clock.tick(60)
+        uav.init()
+        ventana.fill((0,0,0))
+        uav.draw(ventana)
+        pygame.display.flip()
+        #clock.tick(60)
     pygame.quit()
 
 if __name__=="__main__":
