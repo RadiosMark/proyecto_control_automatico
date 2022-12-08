b64fb95a2da10e627edcf513942b5c085f5a7f01
Diego Garcés <dpgarces@uc.cl>
2022-12-08 19:50:40 -0300

    Update control_x.py

--- final/controladores/control_x.py
+++ final/controladores/control_x.py
@@ -14,12 +14,14 @@ theta = np.pi/2
 num1 = [-(g*m*m)/20,-(g*m*((bu-bv)*np.sin(theta) + bv))/20]
 den1 = [20/20, 20200/20, 200500/20, 500000/20, 0 ,0]
 sys1 = tf(num1,den1)
-k = tf([0,200,0],[1,0])
+k = tf([-15,-3,0.0001],[1,0])
 print(k)
 sys2 = feedback(sys1,k)
 
-t_array = np.arange(0,20,0.001)
-y, t = step(sys2,t_array)
+sys3 = series(sys2, tf([-1],[1]))
+
+t_array = np.arange(0,100,0.001)
+y, t = step(sys3,t_array)
 
 fig2 = plt.figure()
 fig2.canvas.set_window_title('Closed loop step response (Ts=0.001)')
@@ -28,3 +30,11 @@ plt.xlabel('t [s]')
 plt.ylabel('y')
 plt.grid()
 plt.show()
+
+
+### revisamos error en régimen permanente
+print(sys3)
+
+error = tf([1, 1010, 1.002e+04, 2.574e+04, 3826, 735.8, 0],[1, 1010, 1.002e+04, 2.574e+04, 3826+ 49.05, 735.8+245.2, 0])
+
+
